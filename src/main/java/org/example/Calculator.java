d7b20465112f9ce047c2460130c0d7f1ccb24127
Senz One <sem63sam@yandex.ru>
2023-11-15 01:15:19 +0400

    created getData

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -8,19 +8,26 @@ public class Calculator {
     private final String input;
     private int a;
     private int b;
+    private char operator;
+    private char charA;
+    private char charB;
+
 
     public Calculator(String input) {
         this.input = input;
     }
 
-    public String calculate() {
+    private void getData() {
         //String input = prompt();
         String[] inputArr = input.split(" ");
 
-        char charA = inputArr[0].charAt(0);
-        char operator = inputArr[1].charAt(0);
-        char charB = inputArr[2].charAt(0);
+        charA = inputArr[0].charAt(0);
+        operator = inputArr[1].charAt(0);
+        charB = inputArr[2].charAt(0);
+    }
 
+    public String calculate() {
+        getData();
         try {
             if (Character.isDigit(charA) && Character.isDigit(charB)) {
                 this.a = Character.getNumericValue(charA);
@@ -28,9 +35,9 @@ public class Calculator {
             } else if ((Character.isDigit(charA) ^ Character.isDigit(charB))) {
                 throw new Exception("Используются одновременно разные системы счисления");
             } else {
-                RomanNumeral romanNumeral1 = RomanNumeral.valueOf(inputArr[0]);
-                this.a = romanNumeral1.getNum();
-                this.b = romanNumeral1.getNum();
+                RomanNumeral romanNumeral = RomanNumeral.valueOf(String.format("%s", charA));
+                this.a = romanNumeral.getNum();
+                this.b = romanNumeral.getNum();
             }
         } catch (Exception e) {
             System.out.println("Ошибка: " + e);
@@ -50,19 +57,19 @@ public class Calculator {
         return null;
     }
 
-    public String sum(int a, int b) {
+    private String sum(int a, int b) {
         return String.format("%s", a + b);
     }
 
-    public String difference(int a, int b) {
+    private String difference(int a, int b) {
         return String.format("%s", a - b);
     }
 
-    public String multiplication(int a, int b) {
+    private String multiplication(int a, int b) {
         return String.format("%s", a * b);
     }
 
-    public String division(int a, int b) {
+    private String division(int a, int b) {
         return String.format("%s", a / b);
     }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator1 = new Calculator("I + I");
         System.out.println(calculator1.calculate());
+
+        Calculator calculator2 = new Calculator("2 + 2");
+        System.out.println(calculator2.calculate());
+
+        Calculator calculator3 = new Calculator("III + 3");
+        System.out.println(calculator3.calculate());
 //
-//        Calculator calculator2 = new Calculator("2 + 2");
-//        System.out.println(calculator2.calculate());
-//
-//        Calculator calculator3 = new Calculator("III + 3");
-//        System.out.println(calculator3.calculate());
-//
-        Calculator calculator4 = new Calculator("1 + 2 + 3");
-        System.out.println(calculator4.calculate());
+//        Calculator calculator4 = new Calculator("1 + 1");
+//        System.out.println(calculator4.calculate());
 
 
     }
