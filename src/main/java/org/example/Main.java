816a3ff62bd5a8f658cf61648b503323c81a7859
Senz One <sem63sam@yandex.ru>
2023-11-20 22:41:48 +0400

    [upd] Task completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator1 = new Calculator("VI - IX");
+        Calculator calculator1 = new Calculator("VI + V");
         calculator1.calculate();
 
-        Calculator calculator2 = new Calculator("11 + 20");
+        Calculator calculator2 = new Calculator("10 + 9");
         calculator2.calculate();
+
+        Calculator calculator3 = new Calculator("10 + 9");
+        calculator3.calculate();
     }
 }
--- src/main/java/org/example/RomanNumeral.java
+++ src/main/java/org/example/RomanNumeral.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 /**
  * Перечисление RomanNumeral представляет римские числа от I до C (1 до 100).
  */
-public enum RomanNumeral {
+enum RomanNumeral {
     I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10), XL(40), L(50), XC(90), C(100);
 
     private final int num;
